bdd917bf1f52a25e554711ca1b3a8c44302f640a
jsoow <digitect38@gmailcom>
2017-02-14 02:31:56 +0900

    test

--- FSharpLearning/fibbo.fs
+++ FSharpLearning/fibbo.fs
@@ -1,10 +1,14 @@
 ﻿//
 // this is the Cool Fibonacci Function Implementation
 ////
+
+
+
+
 module fibbo
 
-let fibbo (n) =     //let fibbo (n:int) =
-    let arr = [|0;1|]     //same as.. let arr:int[] = [|0;1|]
+let fibbo (n) =             //let fibbo (n:int) =
+    let arr = [|0;1|]       //same as.. let arr:int[] = [|0;1|]
     if n > 0 then
         for j = 1 to n/2 do
             arr.[0] <- arr.[0] + arr.[1]
